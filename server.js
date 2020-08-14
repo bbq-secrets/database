763ed75d8bfb9f651f4efe7277bcb54316089430
Artur F C Rosa <artur.corvino1@gmail.com>
2020-08-14 14:39:18 -0300

    enabled only get

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+
+server.all("*", function (req, res, next) {
+  if (req.method === "GET") {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 const router = jsonServer.router("database.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
